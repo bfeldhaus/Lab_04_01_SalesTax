870dfc75a960aa50e6ffd68b040fa69ce1d5313a
brady feldhaus <feldhabl@mail.uc.edu>
2022-02-04 11:34:14 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	int itemCost = 250;
+    double salesTax = 0;
+
+    salesTax = itemCost * 0.05;
+
+    System.out.println("The cost of your item is $" + itemCost + " and sales tax is $" + salesTax);
+    }
+}
